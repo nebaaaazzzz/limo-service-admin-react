4c0f12f218c744c2ccf5220838e4e09a026b9c0a
nebiyu <nebiyuf@gmail.com>
2023-04-19 16:37:47 +0300

    form ui complete

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import LoginPage from "./pages/LoginPage";
 import ForgotPassword from "./pages/ForgotPassword";
 import Register from "./pages/Register";
 import ErrorPage from "./pages/ErrorPage";
+import Account from "./pages/Account";
+import Form from "./pages/Form";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <ErrorPage />;
+  return <Form />;
 }
 
 export default App;
