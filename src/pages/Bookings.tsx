483318fd29d994386acbcf1703df8e847c2f0170
nebiyu <nebiyuf@gmail.com>
2023-04-24 04:41:08 +0300

    dashboard ui in progress

--- src/pages/Bookings.tsx
+++ src/pages/Bookings.tsx
@@ -1,7 +1,101 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
+const reservations = [
+  {
+    vehicleName: "Toyota",
+    vehicleModel: "2021",
+    customerName: "Daniel",
+    status: "completed",
+  },
+  {
+    vehicleName: "Toyota",
+    vehicleModel: "2022",
+    customerName: "Yohannes",
+    status: "completed",
+  },
+  {
+    vehicleName: "Toyota",
+    vehicleModel: "2023",
+    customerName: "Tolosa",
+    status: "completed",
+  },
+  {
+    vehicleName: "Toyota",
+    vehicleModel: "2024",
+    customerName: "Demerew",
+    status: "completed",
+  },
+];
 function Bookings() {
-  return <div>Bookings</div>;
+  return (
+    <>
+      <h4 className="fw-bold py-3 mb-4">
+        <span className="text-muted fw-light">Admin/</span> Book
+      </h4>
+
+      <div className="card">
+        <div className="table-responsive text-nowrap">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Vehicle Name</th>
+                <th>Vehicle Model</th>
+                <th>Customer Name</th>
+                <th>Status</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody className="table-border-bottom-0">
+              {reservations.map(
+                ({ vehicleName, vehicleModel, customerName, status }, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>
+                        <i className="fab fa-react fa-lg text-info me-3"></i>{" "}
+                        <strong>{vehicleName}</strong>
+                      </td>
+                      <td>{vehicleModel}</td>
+                      <td>{customerName}</td>
+                      <td>
+                        <span className="badge bg-label-success me-1">
+                          {status}
+                        </span>
+                      </td>
+                      <td>
+                        <div className="dropdown">
+                          <button
+                            type="button"
+                            className="btn p-0 dropdown-toggle hide-arrow"
+                            data-bs-toggle="dropdown"
+                          >
+                            <i className="bx bx-dots-vertical-rounded"></i>
+                          </button>
+                          <div className="dropdown-menu">
+                            <Link
+                              to={`/reservation/${i}`}
+                              className="dropdown-item"
+                            >
+                              <i className="bx bx-edit-alt me-1"></i> Edit
+                            </Link>
+                            <a
+                              className="dropdown-item"
+                              href="javascript:void(0);"
+                            >
+                              <i className="bx bx-trash me-1"></i> Delete
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Bookings;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,60 @@
 import React from "react";
+import paypalImg from "../assets/img/icons/unicons/paypal.png";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div className="row mb-5">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => {
+          return (
+            <div className="col-md-6 col-lg-4 mt-4">
+              <div className="card">
+                <div className="card-body">
+                  <div className="card-title d-flex align-items-start justify-content-between">
+                    <div className="avatar flex-shrink-0">
+                      <img
+                        src={paypalImg}
+                        alt="Credit Card"
+                        className="rounded"
+                      />
+                    </div>
+                    <div className="dropdown">
+                      <button
+                        className="btn p-0"
+                        type="button"
+                        id="cardOpt4"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        <i className="bx bx-dots-vertical-rounded"></i>
+                      </button>
+                      <div
+                        className="dropdown-menu dropdown-menu-end"
+                        aria-labelledby="cardOpt4"
+                      >
+                        <a className="dropdown-item" href="javascript:void(0);">
+                          View More
+                        </a>
+                        <a className="dropdown-item" href="javascript:void(0);">
+                          Delete
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <span className="d-block mb-1">Payments</span>
+                  <h3 className="card-title text-nowrap mb-2">$2,456</h3>
+                  <small className="text-danger fw-semibold">
+                    <i className="bx bx-down-arrow-alt"></i> -14.82%
+                  </small>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
