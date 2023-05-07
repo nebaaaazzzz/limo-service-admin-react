c561442629eac2418a463cae08d73f2868f5dd81
nebiyu <nebiyuf@gmail.com>
2023-05-07 03:03:59 +0300

    logo aspect ratio fixed

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LogoImg from '../assets/react.svg'
+import LogoImg from "../assets/react.svg";
 function Logo() {
   return (
     <div className="app-brand justify-content-center">
       <Link to="/" className="app-brand-link gap-2">
         <span className="app-brand-logo demo">
-         		<img src={LogoImg} style={{
-         			objectFit :"contain",
-         			width : "10rem",
-         			aspectRation :"1/2"
-         		}}/>
+          <img
+            src={LogoImg}
+            style={{
+              objectFit: "contain",
+              width: "10rem",
+              aspectRatio: "2/1",
+            }}
+          />
         </span>
         <span className="app-brand-text demo text-body fw-bolder"></span>
       </Link>
