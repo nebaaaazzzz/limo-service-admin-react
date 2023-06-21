16df031f5bc9dc96ce832f96fadadf8648fed2d7
nebiyu <nebiyuf@gmail.com>
2023-06-21 13:21:38 +0300

    added

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 import AuthContextProvider from "./context/AuthContext";
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
